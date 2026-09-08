12d22a6a54e59d8e243aeebf5f412cabb132f692
MarcelNota <n3gr1t0.mn@gmail.com>
2026-09-08 11:59:04 +0200

    addition of entity

---
+++ src/main/java/org/example/entity/Weather.java
@@ -0,0 +1,55 @@
+package org.example.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Weather {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String city;
+    private Double temperature;
+    private Double windSpeed;
+
+    public Weather() {
+    }
+
+    public Weather(String city, Double temperature, Double windSpeed) {
+        this.city = city;
+        this.temperature = temperature;
+        this.windSpeed = windSpeed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(Double temperature) {
+        this.temperature = temperature;
+    }
+
+    public Double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(Double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+}
