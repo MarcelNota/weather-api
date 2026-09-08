03c9e76f7959e13af5ef11bc69a5ed85bef8295a
MarcelNota <n3gr1t0.mn@gmail.com>
2026-09-08 12:01:07 +0200

    addition of weatherRepository

---
+++ src/main/java/org/example/repository/WeatherRepository.java
@@ -0,0 +1,7 @@
+package org.example.repository;
+
+import org.example.entity.Weather;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WeatherRepository extends JpaRepository<Weather, Long> {
+}
